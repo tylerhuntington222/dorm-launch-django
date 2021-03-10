2fb7f74f3cf5106192f67e0c6fcff8b216ada898
tyler <tyler@MacBook-Pro-2.athenian.org>
2021-03-09 21:33:54 -0800

    Update time zone

--- config/settings/base.py
+++ config/settings/base.py
@@ -23,7 +23,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "PST"
+TIME_ZONE = "America/Los_Angeles"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
@@ -77,6 +77,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "dorm_launch_django.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "dorm_launch_django.shuttles.apps.ShuttlesConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
