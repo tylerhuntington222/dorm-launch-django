32fa920727def17b2c3e1e25f11c9a5c19b93562
tyler <tyler@MacBook-Pro-2.athenian.org>
2021-03-10 20:18:14 -0800

    Update allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,7 +15,12 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["dorm-launch-django.herokuapp.com"])
+# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["dorm-launch-django.herokuapp.com"])
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # DATABASES
 # ------------------------------------------------------------------------------
